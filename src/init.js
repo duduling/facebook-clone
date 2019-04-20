24c216c5350a682b1cdadae66724f39b9218d96d
DuHyeon <engus983@gmail.com>
2019-04-20 13:43:35 +0900

    FEAT: Completed initial setting
    Completed Initial setting => working the init.js

---
+++ src/init.js
@@ -0,0 +1,13 @@
+// init.js
+
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+function handleListening() {
+  console.log(`✅　Listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
