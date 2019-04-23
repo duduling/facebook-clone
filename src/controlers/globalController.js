8ae44995711f5c6aac9eae433fb4fc34fee76f2f
DuHyeon <engus983@gmail.com>
2019-04-23 16:43:42 +0900

    FEAT: MariaDB insert
    Join -> mariaDB insert process sucess

--- src/controlers/globalController.js
+++ src/controlers/globalController.js
@@ -1,4 +1,6 @@
 // Global Controller.js
+import crypto from "crypto";
+import db from "../db";
 import routes from "../routes";
 
 export const getHome = (req, res) => {
@@ -6,11 +8,47 @@ export const getHome = (req, res) => {
 };
 
 export const postLogin = (req, res) => {
-  console.log(req);
   res.redirect(`/feeds${routes.feedsMain}`);
 };
 
 export const postJoin = (req, res) => {
-  console.log(req);
-  res.redirect(`/feeds${routes.feedsMain}`);
+  const {
+    body: { userName, email, birthYear, birthMonth, birthDay, sex }
+  } = req;
+
+  let {
+    body: { password }
+  } = req;
+
+  let userPwSalt;
+
+  crypto.randomBytes(64, (_, buf) => {
+    crypto.pbkdf2(
+      password,
+      buf.toString("base64"),
+      161602,
+      64,
+      "sha512",
+      (_, key) => {
+        // 비밀번호
+        password = key.toString("base64");
+        // 해당 비밀번호 salt
+        userPwSalt = buf.toString("base64");
+
+        const sql =
+          "INSERT INTO Users(`id`, `pw`, `pw-salt`, `name`, `birth`, `sex`) VALUES(?, ?, ?, ?, ?, ?);";
+        const data = [email, password, userPwSalt, userName, "19931009", sex];
+
+        db.query(sql, data, (error, _, __) => {
+          if (error) {
+            console.log(error);
+            res.redirect(routes.home);
+          } else {
+            console.log(userPwSalt);
+            res.redirect(`/feeds${routes.feedsMain}`);
+          }
+        });
+      }
+    );
+  });
 };
