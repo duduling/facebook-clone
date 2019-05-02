5e8871ac5fce25036752f1dc1a33c1d19d50aeca
DuHyeon <engus983@gmail.com>
2019-05-02 15:10:00 +0900

    FEAT: Edit password page change password
    Added edit-password page password change function

--- src/controlers/userController.js
+++ src/controlers/userController.js
@@ -1,4 +1,5 @@
 // User Controller.js
+import crypto from "crypto";
 import db from "../db";
 import routes from "../routes";
 
@@ -13,6 +14,41 @@ export const getEditPassword = (req, res) => {
   res.render("editUserDetail", {
     pageTile: "Edit Password",
     otherUser: req.user
+  });
+};
+
+export const postEditPassword = (req, res) => {
+  let {
+    body: { newPassword }
+  } = req;
+
+  let userPwSalt;
+
+  crypto.randomBytes(64, (_, buf) => {
+    crypto.pbkdf2(
+      newPassword,
+      buf.toString("base64"),
+      parseInt(process.env.CRYPTO_SECRET, 10),
+      parseInt(process.env.CRYPTO_OPTION1, 10),
+      process.env.CRYPTO_OPTION2,
+      (_, key) => {
+        // 비밀번호
+        newPassword = key.toString("base64");
+        // 해당 비밀번호 salt
+        userPwSalt = buf.toString("base64");
+
+        // 가입 정보 넣기
+        const sql = "UPDATE Users SET `pw` = ?, `pw_salt` = ? where `id`= ?";
+        const data = [newPassword, userPwSalt, req.user.id];
+
+        db.query(sql, data, (error, _, __) => {
+          if (error) {
+            console.log(error);
+          }
+          res.redirect(`/user${routes.editPassword}`);
+        });
+      }
+    );
   });
 };
 
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -5,13 +5,15 @@ import { onlyPrivate } from "../middlewares";
 import {
   getEditProfile,
   getEditPassword,
-  getuserFriends
+  getuserFriends,
+  postEditPassword
 } from "../controlers/userController";
 
 const userRouter = express.Router();
 
+userRouter.get(routes.userFriends, onlyPrivate, getuserFriends);
 userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.get(routes.editPassword, onlyPrivate, getEditPassword);
-userRouter.get(routes.userFriends, onlyPrivate, getuserFriends);
+userRouter.post(routes.editPassword, onlyPrivate, postEditPassword);
 
 export default userRouter;
