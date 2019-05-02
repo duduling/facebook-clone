e9e2ecb7c5261a5e00e643589424bc2555653efd
DuHyeon <engus983@gmail.com>
2019-05-02 17:14:04 +0900

    FEAT: user profile page
    Added user profile change function

--- src/controlers/userController.js
+++ src/controlers/userController.js
@@ -10,6 +10,31 @@ export const getEditProfile = (req, res) => {
   });
 };
 
+export const postEditProfile = async (req, res) => {
+  const {
+    files: { profile, cover }
+  } = req;
+  const userProfileUrl =
+    profile === undefined ? req.user.profile : `/${profile[0].path}`;
+  const userCoverUrl =
+    cover === undefined ? req.user.cover : `/${cover[0].path}`;
+
+  try {
+    // 가입 정보 넣기
+    const sql = "UPDATE Users SET `profile` = ?, `cover` = ? where `id`= ?";
+    const data = [userProfileUrl, userCoverUrl, req.user.id];
+
+    await db.query(sql, data, (error, _, __) => {
+      if (error) {
+        console.log(error);
+      }
+      res.redirect(`/user${routes.editProfile}`);
+    });
+  } catch (error) {
+    res.status(400);
+  }
+};
+
 export const getEditPassword = (req, res) => {
   res.render("editUserDetail", {
     pageTile: "Edit Password",
@@ -24,32 +49,36 @@ export const postEditPassword = (req, res) => {
 
   let userPwSalt;
 
-  crypto.randomBytes(64, (_, buf) => {
-    crypto.pbkdf2(
-      newPassword,
-      buf.toString("base64"),
-      parseInt(process.env.CRYPTO_SECRET, 10),
-      parseInt(process.env.CRYPTO_OPTION1, 10),
-      process.env.CRYPTO_OPTION2,
-      (_, key) => {
-        // 비밀번호
-        newPassword = key.toString("base64");
-        // 해당 비밀번호 salt
-        userPwSalt = buf.toString("base64");
+  try {
+    crypto.randomBytes(64, (_, buf) => {
+      crypto.pbkdf2(
+        newPassword,
+        buf.toString("base64"),
+        parseInt(process.env.CRYPTO_SECRET, 10),
+        parseInt(process.env.CRYPTO_OPTION1, 10),
+        process.env.CRYPTO_OPTION2,
+        (_, key) => {
+          // 비밀번호
+          newPassword = key.toString("base64");
+          // 해당 비밀번호 salt
+          userPwSalt = buf.toString("base64");
 
-        // 가입 정보 넣기
-        const sql = "UPDATE Users SET `pw` = ?, `pw_salt` = ? where `id`= ?";
-        const data = [newPassword, userPwSalt, req.user.id];
+          // 가입 정보 넣기
+          const sql = "UPDATE Users SET `pw` = ?, `pw_salt` = ? where `id`= ?";
+          const data = [newPassword, userPwSalt, req.user.id];
 
-        db.query(sql, data, (error, _, __) => {
-          if (error) {
-            console.log(error);
-          }
-          res.redirect(`/user${routes.editPassword}`);
-        });
-      }
-    );
-  });
+          db.query(sql, data, (error, _, __) => {
+            if (error) {
+              console.log(error);
+            }
+            res.redirect(`/user${routes.editPassword}`);
+          });
+        }
+      );
+    });
+  } catch (error) {
+    res.status(400);
+  }
 };
 
 export const getuserFriends = (req, res) => {
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,19 +1,21 @@
 // User Router.js
 import express from "express";
 import routes from "../routes";
-import { onlyPrivate } from "../middlewares";
+import { onlyPrivate, uploadProfile } from "../middlewares";
 import {
   getEditProfile,
+  postEditProfile,
   getEditPassword,
-  getuserFriends,
-  postEditPassword
+  postEditPassword,
+  getuserFriends
 } from "../controlers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get(routes.userFriends, onlyPrivate, getuserFriends);
+userRouter.post(routes.editProfile, uploadProfile, postEditProfile);
 userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
-userRouter.get(routes.editPassword, onlyPrivate, getEditPassword);
 userRouter.post(routes.editPassword, onlyPrivate, postEditPassword);
+userRouter.get(routes.editPassword, onlyPrivate, getEditPassword);
 
 export default userRouter;
