78a3bba24797681d36f21ce528a8925a7f26b05e
DuHyeon <engus983@gmail.com>
2019-05-10 12:23:38 +0900

    FIX: feedBlock-subMenu click function bug
    feedBlock-subMenu can't find idx and when other element click display none

--- src/assets/js/feedBlock.js
+++ src/assets/js/feedBlock.js
@@ -1,11 +1,11 @@
-import axios from "axios";
-
-const jsFeedBolckBtn = document.getElementById("jsFeedBolckBtn");
-const jsFeedBlockSubMenu = document.getElementById("jsFeedBlockSubMenu");
+// const jsFeedBolckBtn = document.getElementById("jsFeedBolckBtn");
+// const jsFeedBlockSubMenu = document.getElementById("jsFeedBlockSubMenu");
+const feedSection = document.getElementById("feedSection");
 const jsFeedBlockEdit = document.getElementById("jsFeedBlockEdit");
-const jsFeedBlockDelete = document.getElementById("jsFeedBlockDelete");
 
 const postForm = document.createElement("form");
+
+let tempSubMenuDocument;
 
 const handleFeedEdit = event => {
   const targetIdx = event.path.filter(e => {
@@ -15,43 +15,93 @@ const handleFeedEdit = event => {
   console.log(`Edit ${targetIdx}`);
 };
 
-const handleFeedDelte = targetIdx => {
-  postForm.setAttribute("charset", "UTF-8");
-  postForm.setAttribute("method", "Post");
-  postForm.setAttribute("action", `/feeds/${targetIdx}/delete`);
+const handleFeedDelte = event => {
+  const deleteFeedIdx = event.path.filter(e => {
+    return e.id === "jsFeedBlockDelete";
+  })[0].value;
+  console.log(deleteFeedIdx);
+  //   postForm.setAttribute("charset", "UTF-8");
+  //   postForm.setAttribute("method", "Post");
+  //   postForm.setAttribute("action", `/feeds/${deleteFeedIdx}/delete`);
 
-  document.body.appendChild(postForm);
-  postForm.submit();
+  //   document.body.appendChild(postForm);
+  //   postForm.submit();
 };
 
-const checkEvent = async (event, checkId) => {
-  const result = await event.path.filter(e => {
-    return e.id === `${checkId}`;
-  })[0];
+// const checkEvent = async event => {
+//   const result = await event.path.filter(e => {
+//     if (e.id === "jsFeedBlockSubMenu") {
+//       return e.id === "jsFeedBlockSubMenu";
+//     }
 
-  if (result) {
-    handleFeedDelte(result.value);
+//     if (e.id === "jsFeedBlockDelete") {
+//       return e.id === "jsFeedBlockDelete";
+//     }
+
+//     if (e.id === "jsFeedBlockEdit") {
+//       return e.id === "jsFeedBlockEdit";
+//     }
+//   });
+
+//   console.log(result);
+
+//   //   if (result) {
+//   //     handleFeedDelte(result.value);
+//   //   }
+// };
+
+// SubMenu Toggle Function
+const offTheSubMenu = () => {
+  tempSubMenuDocument.style.display = "none";
+  tempSubMenuDocument = null;
+};
+
+const feedSubMenuToggle = event => {
+  const subMenuBtnIdx = event.path[1].attributes.value.value;
+  const subMenuDocument = document.getElementById(
+    `jsFeedBlockSubMenuIdx${subMenuBtnIdx}`
+  );
+
+  if (
+    subMenuDocument.style.display === "none" &&
+    tempSubMenuDocument !== subMenuDocument
+  ) {
+    if (tempSubMenuDocument) {
+      offTheSubMenu();
+    }
+    subMenuDocument.style.display = "grid";
+    tempSubMenuDocument = subMenuDocument;
+  } else {
+    subMenuDocument.style.display = "none";
+    tempSubMenuDocument = null;
   }
 };
 
 const subMenuClickEvent = event => {
-  checkEvent(event, "jsFeedBlockDelete");
-};
-
-const feedSubMenuToggle = () => {
-  if (jsFeedBlockSubMenu.style.display === "none") {
-    jsFeedBlockSubMenu.style.display = "grid";
-  } else {
-    jsFeedBlockSubMenu.style.display = "none";
+  if (event.path[1].id === "jsFeedBolckBtnIdx") {
+    feedSubMenuToggle(event);
+  } else if (tempSubMenuDocument) {
+    offTheSubMenu();
   }
+
+  if (
+    event.path[0].id === "jsFeedBlockDelete" ||
+    event.path[1].id === "jsFeedBlockDelete" ||
+    event.path[2].id === "jsFeedBlockDelete"
+  ) {
+    handleFeedDelte(event);
+  }
+
+  //   console.log(event);
+  //   checkEvent(event, "jsFeedBlockDelete");
 };
 
 const init = () => {
-  jsFeedBolckBtn.addEventListener("click", feedSubMenuToggle);
+  //   jsFeedBolckBtn.addEventListener("click", feedSubMenuToggle);
   jsFeedBlockEdit.addEventListener("click", handleFeedEdit);
   window.addEventListener("click", subMenuClickEvent);
 };
 
-if (jsFeedBolckBtn) {
+if (feedSection) {
   init();
 }
